849f7a1d37e0a2211256e8f13db70a649014db41
Jake Weinstein <xboxfanj@msn.com>
2014-12-22 13:01:03 -0500

    build: clang: use swift and VFP4 flags for krait
    Change-Id: I4c22d6d7d8d2dcf5de26ba3061bba58c74eaf93c
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/clang/arm.mk

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -6,6 +6,12 @@ CLANG_CONFIG_arm_EXTRA_ASFLAGS := \
 CLANG_CONFIG_arm_EXTRA_CFLAGS := \
   -no-integrated-as
 
+ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
+  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
+  CLANG_CONFIG_arm_EXTRA_CFLAGS += $(call cc-option,-mcpu=swift,-mcpu=cortex-a15)
+endif
+
 CLANG_CONFIG_arm_EXTRA_CPPFLAGS := \
   -no-integrated-as
 
