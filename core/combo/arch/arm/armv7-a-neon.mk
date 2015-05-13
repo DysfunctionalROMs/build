0def172a70746e5a5a29ac0c590f45765bd4d7c2
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-05-14 00:09:12 +0200

    Revert "Disable -mcpu=cortex-a15 flag for krait"
    This reverts commit d4137ea69ca8b9aedb2af8400e8ee1207b777ecb.
    
    Conflicts:
    	core/combo/arch/arm/armv7-a-neon.mk

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,18 +7,7 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	# TODO: krait is not a cortex-a15, we set the variant to cortex-a15 so that
-	#       hardware divide operations are generated. This should be removed and a
-	#       krait CPU variant added to GCC/clang.
-ifeq ($(A15_QUAD),true)
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad
-else
-        arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
-endif
-
-	# Fake ARM compiler flags as these processors support LPAE and VFP4 which GCC/clang
-	# doesn't advertise.
-	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
+	arch_variant_cflags := -mcpu=cortex-a15
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
