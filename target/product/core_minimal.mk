eae07afaff7479b25e13f76f058da67e73b05247
InkSlinger666 <kengreenwood420666@gmail.com>
2015-05-26 13:06:18 -0500

    Revert "[2/3] CmHardwareService: add cm hardware jar to system server"
    This reverts commit afb8382952a79e15a7ab38d016702e90103d7654.

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -96,7 +96,6 @@ PRODUCT_BOOT_JARS := \
 
 # The order of PRODUCT_SYSTEM_SERVER_JARS matters.
 PRODUCT_SYSTEM_SERVER_JARS := \
-    org.cyanogenmod.hardware \
     services \
     ethernet-service \
     wifi-service
