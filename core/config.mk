37647fd305a59c2047595b490baa18ac7f1ba9ce
Adnan Begovic <adnan@cyngn.com>
2015-05-26 18:21:15 -0700

    build/core: Inherit vendor/* makefiles for check-api.
    Change-Id: I72d409075192b18c35068566c5687092d45be9b4

--- core/config.mk
+++ core/config.mk
@@ -897,4 +897,12 @@ ifneq ($(SLIM_BUILD),)
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/aosp/sepolicy/sepolicy.mk)
 
+# Include any vendor specific config.mk file
+-include $(TOPDIR)vendor/*/build/core/config.mk
+
+# Include any vendor specific apicheck.mk file
+-include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
