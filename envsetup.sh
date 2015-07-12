c592a434447cdcdfd35831e711df4d22409a65fa
Jmz <jmztaylor@gmail.com>
2015-07-12 17:31:18 +0000

    Change wording of chromium not found message
    Change-Id: I332a4d816897b69b640771ce88f18ec3627255c6

--- envsetup.sh
+++ envsetup.sh
@@ -651,8 +651,7 @@ function lunch()
 
     echo
 
-    if $USE_PREBUILT_CHROMIUM
-    then
+    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
         chromium_prebuilt
     fi
 
@@ -2324,12 +2323,17 @@ function chromium_prebuilt() {
     export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
     hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
 
+    # Colors
+    txtbld=$(tput bold)
+    bldblu=${txtbld}$(tput setaf 4)
+    bldgrn=${txtbld}$(tput setaf 2)
+
     if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ]; then
         export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
         echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
     else
         export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
-        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
+        echo "** Prebuilt Chromium out-of-date/not found or the flag is not set; Will build from source **"
     fi
 }
 
@@ -2364,27 +2368,6 @@ function make()
     echo -e " ####"
     echo
     return $ret
-}
-
-function chromium_prebuilt() {
-    T=$(gettop)
-    export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
-    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
-
-    # Colors
-    txtbld=$(tput bold)
-    bldblu=${txtbld}$(tput setaf 4)
-    bldgrn=${txtbld}$(tput setaf 2)
-
-    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ] && [ -f $libsCheck ] && [ -d $appCheck ]; then
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
-        echo -e ${bldblu}"Prebuilt Chromium is up-to-date: ${bldgrn}Will be used for build"${txtrst}
-        echo -e ""
-    else
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
-        echo -e ${bldblu}"Prebuilt Chromium out-of-date or not found: ${bldgrn}Will build from source"${txtrst}
-        echo -e ""
-    fi
 }
 
 if [ "x$SHELL" != "x/bin/bash" ]; then
