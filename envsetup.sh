4310fdce3fd12af222690ae7cf2efc76dfbf9b6b
InkSlinger666 <kengreenwood420666@gmail.com>
2015-08-01 14:56:24 -0500

    Fix chromium prebuilt to work with the build script

--- envsetup.sh
+++ envsetup.sh
@@ -653,6 +653,9 @@ function lunch()
 
     if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
         chromium_prebuilt
+    else
+        # Unset flag in case user opts out later on
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
     fi
 
     set_stuff_for_environment
