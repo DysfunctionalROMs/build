dec8ad1542630b8dbad1e4bb27bec272bd40ba46
Anthony King <anthonydking@slimroms.net>
2015-09-02 11:01:31 -0700

    build: Allow QC platforms to share/customize HAL variants
    * Allow devices to override HAL variant using QCOM_HARDWARE_VARIANT
    * 8909 and 8916 share HALs by default
    * 8610, 8226, 8974 share HALs by default
    * 8992, 8994 share HALs by default
    
    Change-Id: Idfbdc405a35c5ba518fc71c8b691fc177ddf9068
    
    Conflicts:
    	core/qcom_target.mk

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -1,17 +1,15 @@
+
 # Target-specific configuration
 
 # Populate the qcom hardware variants in the project pathmap.
-define qcom-set-path-variant
-$(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
-endef
 define ril-set-path-variant
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
 endef
-define gps-hal-set-path-variant
-$(call project-set-path-variant,gps-hal,TARGET_GPS_HAL_PATH,$(1))
-endef
-define loc-api-set-path-variant
-$(call project-set-path-variant,loc-api,TARGET_LOC_API_PATH,$(1))
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(call project-set-path,qcom-$(2),$(strip $(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(TARGET_DEVICE_DIR)/$(2), $(3))))
 endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
@@ -47,32 +45,37 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
-TARGET_BOARD_PLATFORM_GENERIC := $(TARGET_BOARD_PLATFORM)
-ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-    TARGET_BOARD_PLATFORM_GENERIC := msm8916
-endif
+    ifeq ($(QCOM_HARDWARE_VARIANT),)
+        ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8974
+        else
+        ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8916
+        else
+        ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8994
+        else
+            QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+        endif
+        endif
+        endif
+    endif
 
-$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM_GENERIC))
-ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
-$(call project-set-path,qcom-camera,$(TARGET_DEVICE_DIR)/camera)
-else
-$(call qcom-set-path-variant,CAMERA,camera)
-endif
-$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM_GENERIC))
-$(call qcom-set-path-variant,GPS,gps)
-$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM_GENERIC))
-$(call qcom-set-path-variant,SENSORS,sensors)
+$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
+$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
+$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
 $(call ril-set-path-variant,ril)
-$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
-$(call gps-hal-set-path-variant,hardware/qcom/gps)
+$(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 else
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
-$(call qcom-set-path-variant,CAMERA,camera)
-$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM_GENERIC))
-$(call qcom-set-path-variant,GPS,gps)
+$(call project-set-path,qcom-camera,hardware/qcom/camera)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-gps,hardware/qcom/gps)
 $(call project-set-path,qcom-media,hardware/qcom/media/default)
-$(call qcom-set-path-variant,SENSORS,sensors)
+$(call project-set-path,qcom-sensors,hardware/qcom/sensors)
 $(call ril-set-path-variant,ril)
-$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
-$(call gps-hal-set-path-variant,hardware/qcom/gps)
+$(call project-set-path,loc-api,vendor/qcom/opensource/location)
 endif
