d2a09c7263b701e6042e2b99d59cf888682a062f
Paul Keith <javelinanddart@gmail.com>
2015-09-30 06:38:25 -0700

    build: Add Flag to Use Oracle JDK on Codefire * Works just fine, easier to deploy Oracle JDK Locally
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -203,9 +203,11 @@ endif
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
 # For Java 1.6, we require Oracle for all host OSes.
 requires_openjdk := false
+ifneq ($(BUILDING_ON_CODEFIRE),true)
 ifeq ($(LEGACY_USE_JAVA6),)
 ifeq ($(HOST_OS), linux)
 requires_openjdk := true
+endif
 endif
 endif
 
