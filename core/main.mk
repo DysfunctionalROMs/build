Evervolv/android_build

build: let's have art use the default settings
 * we dont neet to use verify-at-runtime with ev builds,
   speed will due just fine

Change-Id: I70517e0fdcf2584629613c5bb03901d61eb3ab84

core/main.mk
@@ -366,11 +366,13 @@ ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL
           $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))) \
           ro.setupwizard.mode=OPTIONAL
 endif
-ifndef is_sdk_build
-  # Don't verify or compile the image on eng builds to speed startup.
-  ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-filter=verify-at-runtime
-  # Don't verify or compile apps on eng builds to speed startup.
-  ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.dex2oat-filter=verify-at-runtime
+ifeq ($(EV_BUILD),)
+  ifndef is_sdk_build
+    # Don't verify or compile the image on eng builds to speed startup.
+    ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-filter=verify-at-runtime
+    # Don't verify or compile apps on eng builds to speed startup.
+    ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.dex2oat-filter=verify-at-runtime
+  endif
 endif
   ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.usejit=true
 endif

DysfunctionalROMs/build

 * we dont neet to use verify-at-runtime with broken builds,

core/main.mk
@@ -389,11 +389,13 @@ ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL
           $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))) \
           ro.setupwizard.mode=OPTIONAL
 endif
-ifndef is_sdk_build
-  # Don't verify or compile the image on eng builds to speed startup.
-  ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-filter=verify-at-runtime
-  # Don't verify or compile apps on eng builds to speed startup.
-  ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.dex2oat-filter=verify-at-runtime
+ifeq ($(BROKEN_BUILD),)
+  ifndef is_sdk_build
+    # Don't verify or compile the image on eng builds to speed startup.
+    ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-filter=verify-at-runtime
+    # Don't verify or compile apps on eng builds to speed startup.
+    ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.dex2oat-filter=verify-at-runtime
+  endif
 endif
   ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.usejit=true
 endif