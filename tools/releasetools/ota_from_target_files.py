3f0dc19e9719832d41a3361d11b4127d9e5d46dd
HashBang <hashbang173@gmail.com>
2016-11-22 11:14:08 +0000

    build: Fix support for devices that don't have ro.product.model

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -518,11 +518,21 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
     build = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
     date = GetBuildProp("ro.build.date", OPTIONS.info_dict)
-    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     version = GetBuildProp("ro.cardinal.version", OPTIONS.info_dict)
 
+
+  if GetBuildProp("ro.product.model", OPTIONS.info_dict) is not None:
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
     script.Print("***********************************************");
     script.Print("           Cardinal-AOSP for %s"%(model));
+    script.Print("	   Cardinal-AOSP Version: %s"%(version));
+    script.Print("     AOSP Version: %s"%(build));
+    script.Print("     Compiled on: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = GetBuildProp("ro.product.name", OPTIONS.info_dict)
+    script.Print("***********************************************");
+    script.Print("           Cardinal-AOSP for %s"%(name));
     script.Print("	   Cardinal-AOSP Version: %s"%(version));
     script.Print("     AOSP Version: %s"%(build));
     script.Print("     Compiled on: %s"%(date));
@@ -616,7 +626,8 @@ def GetBuildProp(prop, info_dict):
   try:
     return info_dict.get("build.prop", {})[prop]
   except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
+    print ("WARNING: could not find %s in build.prop" % (prop,))
+  return None
 
 
 def HandleDowngradeMetadata(metadata):
