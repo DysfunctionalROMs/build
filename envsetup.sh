2085756edf48197413cfcb43eb338f624600daa3
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Cardinal build support
    core: Move cardinal sepolicy to device/
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- envsetup.sh
+++ envsetup.sh
@@ -135,6 +135,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^cardinal_") ; then
+        CARDINAL_BUILD=$(echo -n $1 | sed -e 's/^cardinal_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CARDINAL_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        CARDINAL_BUILD=
+    fi
+    export CARDINAL_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -613,6 +621,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
