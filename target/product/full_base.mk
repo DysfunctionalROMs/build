4ff13ac24c3146c4bbe5a0912f5f4403d1d05a11
beanstown106 <nbenis106@gmail.com>
2017-11-09 16:06:37 +0000

    build: Use Google Pixel Audio Files [1/2]
    * Cardinal edits:
    move files and props to vendor
    
    Change-Id: If2fea0ae51e3fe7bb522269ba85b7e46254dcd25
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -37,16 +37,8 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     PhotoTable
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
-
-# Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
