Aos-pure/android_build

core: Include QCOM targets
Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

core/config.mk
@@ -885,4 +885,7 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk

DysfunctionalROMs/build

core/config.mk
@@ -971,6 +971,9 @@ $(eval include device/cardinal/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/citrus/build/core/qcom_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
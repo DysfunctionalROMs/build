8f539f132d45a4bd560d03d59f31add2164ae2d0
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

--- core/config.mk
+++ core/config.mk
@@ -971,6 +971,9 @@ $(eval include device/cardinal/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/citrus/build/core/qcom_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
